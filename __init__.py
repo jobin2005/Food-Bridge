bceb317210c2d26b1f6f8edde8920e3cf79ca78d
Kosen <polarist.light.ray@gmail.com>
2025-03-05 12:23:19 +0530

    Basic Flask Structure Only

---
+++ __init__.py
@@ -0,0 +1,95 @@
+import os
+
+from flask import Flask, render_template, request
+
+def create_app(test_config=None):
+    # create and configure the app
+    app = Flask(__name__, instance_relative_config=True)
+    app.config.from_mapping(
+        SECRET_KEY='dev',
+        DATABASE=os.path.join(app.instance_path, 'flaskr.sqlite'),
+    )
+
+    if test_config is None:
+        # load the instance config, if it exists, when not testing
+        app.config.from_pyfile('config.py', silent=True)
+    else:
+        # load the test config if passed in
+        app.config.from_mapping(test_config)
+
+    # ensure the instance folder exists
+    try:
+        os.makedirs(app.instance_path)
+    except OSError:
+        pass
+
+    # a simple page that says hello
+    @app.route('/hello')
+    def hello():
+        return 'Hello, World!'
+    
+    @app.route('/')
+    def mainpage():
+        return render_template("mainpage.html")
+    
+    @app.route('/contact')
+    def contact():
+        return render_template("contact.html")
+    
+    @app.route('/about')
+    def aboutus():
+        return render_template("aboutus.html")
+    
+    @app.route('/signup', methods=['GET', 'POST'])
+    def signup():
+        if request.method == 'POST':
+            data = request.form
+            print(data)
+        return render_template("signup.html")
+    
+    @app.route('/login', methods=['GET', 'POST'])
+    def login():
+        return render_template("login.html")
+    
+    @app.route('/register')
+    def register():
+        return render_template("register.html")
+    
+    @app.route('/feedback')
+    def feedback():
+        return render_template("feedback.html")
+    
+    @app.route('/support')
+    def support():
+        return render_template("support.html")
+    
+    @app.route('/donor')
+    def donor():
+        return render_template("donor.html")
+    
+    @app.route('/donor_profile')
+    def donor_profile():
+        return render_template("donor_profile.html")
+    
+    @app.route('/ngo')
+    def ngo():
+        return render_template("ngo.html")
+    
+    @app.route('/ngo_profile')
+    def ngo_profile():
+        return render_template("ngo_profile.html")
+    
+    @app.route('/volunteer')
+    def volunteer():
+        return render_template("volunteer.html")
+    
+    @app.route('/volunteer_profile')
+    def volunteer_profile():
+        return render_template("volunteer_profile.html")
+
+    return app
+
+
+if __name__ == "__main__":
+    app = create_app()
+    app.run()
